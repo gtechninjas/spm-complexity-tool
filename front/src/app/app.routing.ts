8907aaae1ced97a1f3f0abca5919556b70ad0b89
navod80 <navod80@gmail.com>
2019-07-28 20:19:19 +0100

    Basic Home Page Done

---
+++ front/src/app/app.routing.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import Containers
+import { DefaultLayoutComponent } from './containers';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      {
+        path: '',
+        loadChildren: () => import('./views/form/form.module').then(m => m.FormModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}

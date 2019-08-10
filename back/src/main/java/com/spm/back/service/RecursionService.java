a50f2fafcc4370e0e3842fdac846ec43cb5377e7
suren <surenanthonyvithanage@gmail.com>
2019-08-10 21:40:06 +0530

    Recursion

---
+++ back/src/main/java/com/spm/back/service/RecursionService.java
@@ -0,0 +1,11 @@
+package com.spm.back.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public interface RecursionService {
+	List<String> getAllMethods(File file);
+	Boolean checkRecursionPresent(File file) throws FileNotFoundException, IOException;
+}
---
+++ back/src/main/java/com/spm/back/service/impl/RecursionServiceImpl.java
@@ -0,0 +1,72 @@
+package com.spm.back.service.impl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.spm.back.service.RecursionService;
+
+public class RecursionServiceImpl implements RecursionService {
+
+	@Override
+	public List<String> getAllMethods(File file) {
+		List<String> getMethods = new ArrayList<>();
+		try {
+			// create class object
+			Class classobj = File.class;
+
+			// get list of methods
+			Method[] methods = classobj.getMethods();
+
+			// get the name of every method present in the list
+			for (Method method : methods) {
+
+				String MethodName = method.getName();
+				getMethods.add(MethodName);
+
+			}
+			return getMethods;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public Boolean checkRecursionPresent(File file) throws FileNotFoundException, IOException {
+		List<String> list = new ArrayList<>();
+		List<String> methodList = new ArrayList<>();
+		int recursionCall = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			list = br.lines().collect(Collectors.toList());
+			methodList = getAllMethods(file);
+			recursionCall = getDupCount(methodList);
+			if ( recursionCall > 0 ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public int getDupCount(List<String> l) {
+		int cnt = 0;
+		HashSet<String> h = new HashSet<String>(l);
+
+		for (String token : h) {
+			if (Collections.frequency(l, token) > 1)
+				cnt++;
+		}
+
+		return cnt;
+	}
+
+}

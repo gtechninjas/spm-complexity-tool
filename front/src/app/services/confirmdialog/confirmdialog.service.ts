428062cf7287e47cad7ea04c0a320513bccf3f11
suren <surenanthonyvithanage@gmail.com>
2019-08-10 13:37:09 +0530

    updated

---
+++ front/src/app/services/confirmdialog/confirmdialog.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ConfirmComponent } from '../../components/pages/confirm/confirm.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmdialogService {
+
+  constructor(private modalService: NgbModal) { }
+
+  public confirm(
+    title: string,
+    message: string,
+    btnOkText: string = 'OK',
+    btnCancelText: string = 'Cancel',
+    dialogSize: 'sm'|'lg' = 'sm'): Promise<boolean> {
+    const modalRef = this.modalService.open(ConfirmComponent, { size: dialogSize });
+    modalRef.componentInstance.title = title;
+    modalRef.componentInstance.message = message;
+    modalRef.componentInstance.btnOkText = btnOkText;
+    modalRef.componentInstance.btnCancelText = btnCancelText;
+
+    return modalRef.result;
+  }
+}
---
+++ front/src/app/utility/global/global.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+import { MatCheckboxModule, MatInputModule } from '@angular/material';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { NgbModule  } from '@ng-bootstrap/ng-bootstrap';
+import { MatIconModule } from '@angular/material';
+import { ConfirmComponent } from '../../components/pages/confirm/confirm.component';
+
+
+@NgModule({
+  declarations: [
+    ConfirmComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    FormsModule ,
+    ReactiveFormsModule,
+    NgbModule,
+    MatIconModule
+  ],
+  entryComponents: [
+    ConfirmComponent
+  ]
+})
+export class GlobalModule { }

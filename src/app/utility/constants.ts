e6ef7e4a07f8a9c0da6de22948f990d65ccc1ca4
navod80 <navod80@gmail.com>
2019-08-12 23:19:06 +0530

    Revert "Operator UI commit"
    This reverts commit bb0ad3675345b33153c370ad4a79af32a704d25f.

--- src/app/utility/constants.ts
+++
@@ -1,7 +0,0 @@
-export const HOST = 'localhost';
-export const PORT = '9090';
-
-// true or false as the parameter
-export const getEndpoint = (isHttps) => {
-  return `${isHttps ? 'https' : 'http'}://${HOST}:${PORT}`;
-};

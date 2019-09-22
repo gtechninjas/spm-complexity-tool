26944f6e3eb111874df8fecbee8b5122a61a027e
suren <surenanthonyvithanage@gmail.com>
2019-09-22 07:57:49 +0530

    Completed

--- front/src/app/views/dashboard/dashboard.component.ts
+++ front/src/app/views/dashboard/dashboard.component.ts
@@ -10,6 +10,18 @@ import { DashboardService } from '../../service/dashboard/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
+  public recursionList: any = [];
+  public inheritanceList: any = [];
+  public controlList: any = [];
+  public nestedList: any = [];
+  public sizeList: any = [];
+
+  public totalRecursion: number;
+  public totalInheritance: number;
+  public totalControl: number;
+  public totalNested: number;
+  public totalSize: number;
+
   public filePath: string;
   public result: boolean;
 
@@ -31,6 +43,12 @@ export class DashboardComponent implements OnInit {
   ) {
     this.filePath = '';
     this.result = false;
+
+    this.totalRecursion = 0;
+    this.totalInheritance = 0;
+    this.totalControl = 0;
+    this.totalNested = 0;
+    this.totalSize = 0;
   }
 
   ngOnInit() {
@@ -39,6 +57,11 @@ export class DashboardComponent implements OnInit {
 
   _prepare() {
     this.dashboardList();
+    this.getRecursion();
+    this.getInheritance();
+    this.getControlStructure();
+    this.getNestingControlStructure();
+    this.getSize();
   }
 
   dashboardList() {
@@ -91,6 +114,88 @@ export class DashboardComponent implements OnInit {
 
   onSubmit() {
     // this.dashboardListParam();
+  }
+
+  getRecursion() {
+    this.dashboardService.getRecursion().then(
+      data => {
+        this.recursionList = data;
+        console.log('RECURSION : ' + JSON.stringify(this.recursionList));
+        let i;
+        for ( i = 0 ; i < this.recursionList.length; i++){
+          this.totalRecursion += Number(this.recursionList[i]);
+        }
+      },
+      err => {
+
+      }
+    )
+  }
+
+  getInheritance() {
+    this.dashboardService.getInheritance().then(
+      data => {
+        this.inheritanceList = data;
+        console.log('Inheritance : ' + JSON.stringify(this.inheritanceList));
+        let i;
+        for ( i = 0 ; i < this.inheritanceList.length; i++){
+          this.totalInheritance += Number(this.inheritanceList[i]);
+        }
+      },
+      err => {
+
+      }
+    )
+  }
+
+  getControlStructure() {
+    this.dashboardService.getControlStucture().then(
+      data => {
+        this.controlList = data;
+        console.log('CONTROL STRUCTURE : ' + JSON.stringify(this.controlList));
+        let i;
+        for ( i = 0 ; i < this.controlList.length; i++){
+          this.totalControl += Number(this.controlList[i]);
+        }
+      },
+      err => {
+
+      }
+    )
+  }
+
+
+  getNestingControlStructure() {
+    this.dashboardService.getNestingControlStucture().then(
+      data => {
+        this.nestedList = data;
+        console.log('NESTED : ' + JSON.stringify(this.nestedList));
+        let i;
+        for ( i = 0 ; i < this.nestedList.length; i++){
+          this.totalNested += Number(this.nestedList[i]);
+        }
+      },
+      err => {
+
+      }
+    )
+  }
+
+
+  getSize() {
+    this.dashboardService.getSize().then(
+      data => {
+        this.sizeList = data;
+        console.log('SIZE : ' + JSON.stringify(this.sizeList));
+        let i;
+        for ( i = 0 ; i < this.sizeList.length; i++){
+          this.totalSize += Number(this.sizeList[i]);
+        }
+      },
+      err => {
+
+      }
+    )
   }
 
 }
